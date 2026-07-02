ecdeec159e1883c9be4ecd5e5e4d588e6bc016b7
Protais Nkundabagenzi <nkundabagenzi.protais@gmail.com>
2026-07-02 11:32:29 +0200

    Fix cPanel Laravel backend path

--- infra/cpanel/api-front-controller.php
+++ infra/cpanel/api-front-controller.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-$backendPath = dirname(__DIR__, 2) . '/backend';
+$backendPath = dirname(__DIR__) . '/backend';
 
 if (! file_exists($backendPath . '/vendor/autoload.php')) {
     http_response_code(503);
