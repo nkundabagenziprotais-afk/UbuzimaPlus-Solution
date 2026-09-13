fec28dc59bf4bdbfd3c9de6289e4b634216f42af
Protais Gasana <nkundabagenzi.protais@gmail.com>
2026-09-13 19:22:03 +0200

    release(mobile): use commercial preview identity

--- mobile/ubuzima_native/android/app/build.gradle.kts
+++ mobile/ubuzima_native/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.ubuzimaplus.nativeuat"
+    namespace = "com.ubuzimaplus.preview"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,24 +15,16 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ubuzimaplus.nativeuat"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.ubuzimaplus.preview"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Preview CI build is re-signed with the stable Ubuzima+ Preview key before distribution.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
